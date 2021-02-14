d92b1bf25aa7cd639f69bbd8acbd0f47f8e9f0bb
simonojok19 <simonojok19@gmail.com>
2021-02-14 18:39:33 +0300

    finished the login button

---
+++ resources/js/Pages/Login.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="form-container">
+        <div  class="card" style="width: 40%;">
+            <h2 class="ml-auto mr-auto mb-5">Sign In</h2>
+            <form>
+                <div class="form-group">
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" id="password" placeholder="Password">
+                </div>
+                <button type="submit" class="btn">Login to Clocking</button>
+            </form>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+    .form-container {
+        display: flex;
+        flex-direction: column;
+        position: fixed;
+        justify-content: center;
+        align-content: center;
+        justify-items: center;
+        align-items: center;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        top: 0;
+    }
+
+    .card {
+        padding: 2.5%;
+        background-color: #ebeff0;
+    }
+
+    button {
+        background-color: #18bb9b;
+        color: white;
+        width: 150px;
+        align-self: flex-end;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+
+</style>
--- resources/js/Router/routes.js
+++ resources/js/Router/routes.js
@@ -2,12 +2,27 @@ const routes = [
     {
         path: '',
         component: () => import('../Pages/Home.vue'),
-        name: 'home'
+        name: 'home',
+        meta: {
+            requiresAuth: true
+        }
     },
     {
         path: '/login',
-        component: () => import('../Pages/Login.Vue'),
-        name: 'login'
+        component: () => import('../Pages/Login.vue'),
+        name: 'login',
+        meta: {
+            guest: true
+        }
+    },
+    {
+        path: '/dashboard',
+        component: () => import('../Pages/Dashboard.vue'),
+        name: 'dashboard',
+        meta: {
+            is_admin: true,
+            requiresAuth: true
+        }
     }
 ];
 
